38ea4d204c7a3a52a594d2361f418bbc4e3f6bed
Anty-Anty <79324714+Anty-Anty@users.noreply.github.com>
2025-09-17 12:35:07 -0700

    CORS error adjustments

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 //CORS error fix
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', "https://todoapp-frontend-trxp.onrender.com", '*');
+    res.setHeader('Access-Control-Allow-Origin', '*', process.env.ONRENDER_HEADER);
     res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE')
     next();
